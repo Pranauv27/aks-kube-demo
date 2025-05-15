9edd06283e981e7197f0e14385d888feafa28772
Pranauv27 <pranauvsudha@gmail.com>
2025-05-15 20:49:45 +0530

    modified providers file and updated workflow

--- providers.tf
+++ providers.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
+  use_oidc = true
+}
+
+provider "azapi" {
+  use_oidc = true
 }
 
 terraform {
@@ -9,13 +13,22 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.67.0"
     }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.30.0"
+    }
   }
   backend "azurerm" {
-    subscription_id      = ""
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
+    # subscription_id      = ""
+    # resource_group_name  = ""
+    # storage_account_name = ""
+    # container_name       = ""
     key                  = ""
+    use_oidc             = true
   }
   required_version = ">= 0.14"
 }
