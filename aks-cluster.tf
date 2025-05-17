260c64dc7468c3539ab645f6e087abfe9edfc6fc
Pranauv27 <pranauvsudha@gmail.com>
2025-05-17 10:04:09 +0530

    updating kubernets version and azurerm version

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${var.resource_prefix}-k8s"
-  kubernetes_version  = "1.26.3"
+  kubernetes_version  = "1.30.0"
 
   default_node_pool {
     name            = "${var.resource_prefix}np01"
